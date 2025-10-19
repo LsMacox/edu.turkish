784a863f83fffc9aca2d0a59aa7156af75458abc
Adam Zugiraev <adam.zugiraev@onlinesup.com>
2025-10-19 23:41:52 +0300

    Update configuration files and documentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
+  ignores: ['.nuxt-test/**'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-dynamic-delete': 'off',
